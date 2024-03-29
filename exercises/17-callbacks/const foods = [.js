e23e427f5682b79ddb423089e5e6ff735c01793d
Danielle Smith <moonart27.ds@gmail.com>
2024-03-29 15:15:53 -0400

    done

---
+++ exercises/17-callbacks/const foods = [.js
@@ -0,0 +1,31 @@
+const foods = [
+    "Apple",
+    "Banana",
+    "Carrot",
+    "Dates",
+    "Eggplant",
+    "Fig",
+    "Grapes",
+    "Honeydew melon",
+    "Iceberg lettuce",
+    "Jalapeno",
+    "Kale",
+    "Lentils",
+    "Mango",
+    "Nectarine",
+    "Olive",
+    "Papaya",
+    "Quinoa",
+    "Raspberry",
+    "Spinach",
+    "Tomato",
+    "Ugli fruit",
+    "Vanilla bean",
+    "Watermelon",
+]
+ 
+const printFood = (food, i) => {
+    console.log('${i + 1}. ${food}')
+}
+
+foods.forEach
