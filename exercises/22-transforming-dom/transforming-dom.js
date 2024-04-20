b7930a1c854932eef8ab24b14963f776348528c0
Danielle Smith <moonart27.ds@gmail.com>
2024-04-20 18:34:49 -0400

    catching up

--- exercises/22-transforming-dom/transforming-dom.js
+++ exercises/22-transforming-dom/transforming-dom.js
@@ -4,5 +4,25 @@
  * You may modify the HTML to add ids, classes, data attributes, etc.
  */
 (function () {
-  // Put your answers in here
+  const image = document.querySelector("#image");
+  image.src = "https://media.giphy.com/media/3oKIPnAiaMCws8nOsE/giphy.gif";
+
+  document.querySelector("#javascriptLink").href =
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
+
+  document.querySelector("#victorious").textContent = "I am victorious!";
+
+  document.querySelector("#inlineStyles").style.backgroundColor = "white";
+
+  const dangerNoMore = document.querySelector("#dangerNoMore");
+  dangerNoMore.classList.add("text-dark");
+
+  document.querySelector("#hideMe").style.display = "none";
+
+  document.querySelector("#abraKadabra").classList.remove("hidden");
+
+  const button = document.querySelector("#thisButton");
+  if (button.classList.contains("btn-primary")) {
+    document.querySelector("#buttonOutput").textContent = "✓ blue";
+  }
 })();
